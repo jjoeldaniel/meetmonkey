eac058f10ca59da2a4adaa9dc65f99303879d43d
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-03-13 20:50:13 -0700

    MONKE

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import openai
 import os
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 
@@ -16,17 +17,24 @@ hobbies.
 '''
 
 
+def random_monkey():
+    first_names = ['Alice', 'Bob', 'Charlie', 'David', 'Emma', 'Frank', 'Grace', 'Henry', 'Isabella', 'Jack', 'Katherine', 'Liam', 'Mia', 'Nathan', 'Olivia', 'Penelope', 'Quinn', 'Robert', 'Sophia', 'Thomas', 'Uma', 'Victoria', 'William', 'Xavier', 'Yara', 'Zachary']
+    return random.choice(first_names)
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     # Generate response
+    name = random_monkey()
     response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
         messages=[
             {'role': 'system', 'content': initial_prompt},
-            {'role': 'user', 'content': "Create a fake bio for a monkey"}
+            {'role': 'user', 'content': f"Create a fake bio for this monkey named {name}. Write this in the first person. Only include the bio and no labels"}
         ]
     )
 
     reply = response['choices'][0]['message']['content']
-    return render_template('index.html', bio=reply)
+    print(reply)
+    return render_template('index.html', name=name, bio=reply)
